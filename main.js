7c6b1118a4ad1d40297e7038e291f8cace4e3d2d
Ruben Ortega <ruben000_2@live.com>
2024-01-04 10:58:35 -0800

    Minor changes to formatting of annual pay

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+document.getElementById("resultLabel").addEventListener("input", function () {
+  getHourlyWage();
+});
+
 document.getElementById("hourlyWage").addEventListener("input", function () {
   getHourlyWage();
 });
@@ -14,10 +18,13 @@ function getHourlyWage() {
   var hourlyWageValue = document.getElementById("hourlyWage").value;
   var weeks_in_Year_Value = document.getElementById("weeks_in_Year").value;
   var weeklyHours = document.getElementById("weeklyHours").value;
-  var anualIncome = hourlyWageValue * weeks_in_Year_Value * weeklyHours;
+  var annualIncome = hourlyWageValue * weeks_in_Year_Value * weeklyHours;
+  annualIncome = annualIncome.toFixed(2);
+
+  var formattedIncome = parseFloat(annualIncome).toLocaleString();
 
   var resultLabel = document.getElementById("resultLabel");
-  resultLabel.textContent = "Result: " + anualIncome;
+  resultLabel.textContent = "Annual Pay: $" + formattedIncome;
 
   // You can use the hourlyWageValue variable in your JavaScript logic
 }
